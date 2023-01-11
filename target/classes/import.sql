5e98f317d9927308898d843da7631a3a7e184182
yelose <yelose85@gmail.com>
2023-01-11 08:47:15 +0100

    added lectores imports

--- target/classes/import.sql
+++ target/classes/import.sql
@@ -1,2 +1,5 @@
 INSERT INTO `biblioteca`.`libros` (`anyo`, `autor`, `editorial`, `genero`, `nombre_libro`, `numero_copias`) VALUES ('1950', 'pepe', 'obelisco', 'misterio', 'Paquita', '3');
 INSERT INTO `biblioteca`.`libros` (`anyo`, `autor`, `editorial`, `genero`, `nombre_libro`, `numero_copias`) VALUES ('1989', 'pepa', 'sirio', 'alternativo', 'Pepa', '2');
+INSERT INTO `biblioteca`.`lectores` (`direccion`, `n_libros_prestados`, `nombre_lector`, `telefono`) VALUES ('jalksjd', '0', 'Cosa', '123451234');
+INSERT INTO `biblioteca`.`lectores` (`direccion`, `n_libros_prestados`, `nombre_lector`, `telefono`) VALUES ('aSddfF', '1', 'Algo', '234566543');
+INSERT INTO `biblioteca`.`lectores` (`direccion`, `n_libros_prestados`, `nombre_lector`, `telefono`) VALUES ('Vaso de Agua', '0', 'Mesa', '234567885');
